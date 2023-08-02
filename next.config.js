2f828ef1f59053caadd2647ccf29350b738d4f7c
Blanche Thome <blanchet42196@gmail.com>
2023-08-02 16:31:39 +0200

    Build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    generateStaticParams: function () {
+        return {
+          '/': { page: '/' }, // Add your custom routes here if needed
+        };
+      },
+}
+
+
 
 module.exports = nextConfig
