5bee9e5439c7378e460335e4e06676f562c87583
Blanche Thome <blanchet42196@gmail.com>
2023-08-05 14:29:57 +0200

    Satistic

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 "use client";
 
+/* eslint-disable */
+/* eslint-disable no-unused-vars, react/no-children-prop */
+/* eslint-disable */
+
 import Image from 'next/image';
 import { useState } from 'react';
 import Navbar from './navbar';
@@ -13,26 +17,42 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <Title children={"Bienvenue à la casa rural de la Portella"}></Title>
-      <div className="flex ml-8 rounded-lh">
+
+      <div className="grid lg:grid-cols-3 gap-4 rounded-lh mt-11 mb-11 place-items-stretch">
         <Image
-          className='rounded-3xl mt-11 mb-11'
-          src="https://media.er2.co/es/lleida/0000000016775/635/516d1e8ae292b.jpg"
+          className='rounded-3xl grid bg-white'
+          src="https://media.er2.co/es/lleida/0000000016775/1600/5489a0c7dec29.jpg"
           alt="JACUZZI"
-          width={350}
+          width={650}
           height={100}
         />
         <Image
-          className='rounded-3xl mt-11 mb-11 ml-4' 
+          className='rounded-3xl grid bg-white'
           src="https://media.er2.co/es/lleida/0000000016775/1600/5040875544a6f.jpg"
           alt="MAISON"
           width={650}
           height={100}
         />
         <Image
-          className='rounded-3xl mt-11 mb-11 ml-4' 
+          className='rounded-3xl grid bg-white'
           src="https://media.er2.co/es/lleida/0000000016775/1600/53b9011c9e46f.jpg"
           alt="PISCINE"
-          width={350}
+          width={650}
+          height={100}
+        />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="https://media.er2.co/es/lleida/0000000016775/1600/5f628610463f3.jpg"
+          alt="PISCINE"
+          width={650}
+          height={100}
+        />
+        
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="https://i.postimg.cc/VNrMBwXK/Capture-d-e-cran-2023-08-05-a-13-32-52.png"
+          alt="PISCINE"
+          width={650}
           height={100}
         />
       </div>
