bcfefd18a36ae4885667c9c0b28f13f2e912b409
Blanche Thome <blanchet42196@gmail.com>
2023-08-05 15:58:26 +0200

    fix2

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Title children={"Organisation"}/>
+      <Title children={"Accueil"}/>
 
       <div className="grid lg:grid-cols-3 gap-4 rounded-lh mt-11 mb-11 place-items-stretch">
-        <Image
+        {/* <Image
           className='rounded-3xl grid bg-white'
           src="https://media.er2.co/es/lleida/0000000016775/1600/5489a0c7dec29.jpg"
           alt="JACUZZI"
@@ -42,15 +42,51 @@ export default function Home() {
           alt="PISCINE"
           width={650}
           height={100}
-        />
-        
+        /> */}
         <Image
           className='rounded-3xl grid bg-white'
-          src="https://i.postimg.cc/VNrMBwXK/Capture-d-e-cran-2023-08-05-a-13-32-52.png"
-          alt="PISCINE"
+          src="/861f63b4-9ef1-4b41-a719-178d99203a4e.jpg"
+          alt="FAMILIA"
           width={650}
           height={100}
         />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="/384aaf43-2c0b-4b00-b6a5-66bba05ee1a1.jpg"
+          alt="FAMILIA"
+          width={650}
+          height={100}
+        />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="/1600646b-43ce-45de-8331-19a7b3f71a27.jpg"
+          alt="FAMILIA"
+          width={650}
+          height={100}
+        />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="/fe87740f-bbc3-4d9f-bf6a-bb9f03163f39.jpg"
+          alt="FAMILIA"
+          width={650}
+          height={100}
+        />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="/3176f969-10d6-419b-ae44-d9f3f2a80a67.jpg"
+          alt="FAMILIA"
+          width={650}
+          height={100}
+        />
+        <Image
+          className='rounded-3xl grid bg-white'
+          src="/284a974f-48eb-4d70-ba61-cf439a730284.jpg"
+          alt="FAMILIA"
+          width={650}
+          height={100}
+        />
+        
+        
       </div>
     </div>
   );
