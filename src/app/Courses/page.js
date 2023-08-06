95931dad8c35e837974e4f859bdadcd3861d4984
hugoroussel <hroussel96@gmail.com>
2023-08-06 16:29:30 +0200

    DB

---
+++ src/app/Courses/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import Navbar from "../navbar";
+import Title from "../title";
+
+export default function Courses() {
+
+    return(
+        <div>
+            <Navbar/>
+            <Title children={"Listes de Courses"}/>
+
+            Coming soon...
+
+        </div>
+    )
+
+}
